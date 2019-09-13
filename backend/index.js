13bad8a24f5efa0d87cccc7e621343fa59903d35
Abhinash PC <avinash16595@gmail.com>
2019-09-13 21:09:15 +0530

    new files in frontend

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ const app = express();
 const chalk = require('chalk');
 
 var cors = require('cors')
- app.use(cors())
- 
+app.use(cors())
+
 //include the lodash & underscore
 const _ = require('underscore');
 
@@ -23,7 +23,6 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
 	next();
   });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(chalk.red.bgWhite.bold(`Listening on port ${port}...`), chalk.green('✓')));
