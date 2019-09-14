ef56aae1eca222166b97b6724c062abc0a6e4b36
Abhinash PC <avinash16595@gmail.com>
2019-09-14 13:12:05 +0530

    updates

--- frontend-quiz/src/App.vue
+++ frontend-quiz/src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="app">
-      <h1>Play Quiz</h1>
-      <p>Your Score: {{Score}}</p>
-      <p>{{questions.question}}</p>
+    <div class="container-fluid text-center">
+      <h1>PlayQuiz.com</h1>
+    </div>
+    <div  v-show="startQuiz" class="container-fluid text-center">
+     <p> Welcome!!</p>
+        <button v-on:click="start" class="btn btn-primary">Let's get started!!</button><br><br>
+    </div>
 
-      <div v-for="answer in questions.options">
-        <input type="radio" v-model="Response" name="options" :value="answer.isCorrect" v-on:click="validate" :id="answer.id">{{answer.option}}<br/>
-      </div>
-
-      <button v-on:click="prev">Previous</button>
-      <button v-on:click="next">Next</button><br><br>
-
-        <span>{{Response}}</span>
+<div v-show="showQuestion && currentQuestion < datas.length - 1" class="container-fluid">
+  <p id="question">Q{{currentQuestion + 1}} {{datas[currentQuestion].question}}</p>
+  <div v-for="answer in datas[currentQuestion].options">
+     <div class="radio">
+      <input type="radio" v-model="Response" name="options" :value="answer.isCorrect" v-on:click="next" :id="answer.id">
+      {{answer.option}}
+    </div>
   </div>
-</template>
+</div>
 
+<div v-show="show" class="container text-center">
+  <div v-show="Score < 5">
+    <p>Better Luck Next Time!!</p>
+    <p>Your Score : {{Score}}</p>
+  </div>
+  <div v-show="Score > 5 ">
+  <p>Tada you are a Champion!!</p>
+  <p>Your Score : {{Score}}</p>
+</div>
+</div>
+</div>
+
+</template>
 <script>
 import VueResource from 'vue-resource'
 
@@ -24,42 +40,57 @@ export default {
     return {
       datas: [],
       questions:[],
-      index:0,
+      currentQuestion:0,
       Response:[],
-      Score: 0
+      Score: 0,
+      startQuiz: false,
+      showQuestion:false,
+      showResult:false,
+      show:false
     }
   },
   created(){
       this.$http.get('http://localhost:8080/api/questions')
       .then(response=>{
         this.datas = response.body;
-        this.questions = this.datas[this.index]
+        // this.questions = this.datas[this.currentQuestion]
+        // console.log(this.datas[this.currentQuestion].question)
+        this.startQuiz = true;
       })
   },
 methods:{
+  start: function(){
+    this.startQuiz = false;
+    this.showQuestion = true;
+  },
+
   prev: function(){
-    this.index--
+    this.currentQuestion--
   },
 
   next: function(){
-    this.index++
-  },
-  validate: function (){
-    console.log('Work')
 
-      if(this.checkedNames == true){
-      console.log('Correct')
-
-      }else if(this.checkedNames == false){
-      console.log('Correct')
-      }
+    if(this.response == true){
+      this.Score = this.Score + 3
+    }else{
+      this.Score = this.Score -1
     }
+    this.currentQuestion++
+    if(this.currentQuestion == this.datas.length - 1){
+      this.show = true;
+    }
+
+  },
   }
 }
 </script>
 
 <style scoped>
-#app h1{
-  text-align: center
+#question{
+ text-align: center;
+ padding:5px 5px 5px 5px;
+}
+.radio{
+  padding: 5px 5px 5px 450px;
 }
 </style>
